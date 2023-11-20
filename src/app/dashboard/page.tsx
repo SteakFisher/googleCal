7cb9e5b29f79d2df87471dde7d55050225eafbbc
SteakFisher <miniminter2018p@gmail.com>
2023-11-20 07:38:53 +0530

    Made the event's page function

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,8 @@
-'use client'
-
 import Link from "next/link";
-import {useEffect} from "react";
-import {redirect} from "next/navigation";
 
 let flag = false;
 
 export default function Dashboard() {
-    console.log('ye')
-    useEffect(() => {
-        redirect('/event')
-    }, []);
     return (
         <Link href={"/event"}>
             <button>New Calendar event</button>
--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -2,6 +2,8 @@
 
 import {ChangeEvent, useState} from "react";
 import cloneStructure from "../../../functions/cloneStructure";
+import { Database } from '../../../types/supabase'
+import {createClient} from "@supabase/supabase-js";
 
 type Key = 'start' | 'end' | 'description' | 'summary'
 
@@ -13,7 +15,13 @@ type Events = {
 }
 
 export default function Event() {
+    const supabase = createClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    )
+
     const [event, setEvent] = useState({} as Events);
+    const [error, setError] = useState("");
     function onElemChange(e: ChangeEvent, key: Key) {
         // @ts-ignore
         event[key] = e.target.value
@@ -24,34 +32,65 @@ export default function Event() {
         <>
             <h1>Add New Calendar Event</h1>
             <form>
+
+                <label>
+                    TimeZone: {Intl.DateTimeFormat().resolvedOptions().timeZone}
+                </label>
+                <br />
+
                 <label>Title</label>
-                <input type={'text'} onChange={(e) => {
+                <input className={"bg-slate-800"} type={'text'} onChange={(e) => {
                     onElemChange(e, 'summary');
                 }}/>
 
                 <br />
 
                 <label>Start Time</label>
-                <input type={"datetime-local"} onChange={(e) => {
+                <input className={"bg-slate-800"} type={"datetime-local"} onChange={(e) => {
                     onElemChange(e, 'start');
                 }}/>
 
                 <label>End Time</label>
-                <input type={"datetime-local"} onChange={(e) => {
+                <input className={"bg-slate-800"} type={"datetime-local"} onChange={(e) => {
                     onElemChange(e, 'end');
                 }}/>
 
                 <br />
 
                 <label>Description</label>
-                <input type={'text'} onChange={(e) => {
+                <input className={"bg-slate-800"} type={'text'} onChange={(e) => {
                     onElemChange(e, 'description');
                 }}/>
 
+                {
+
+                    error ? (
+                        <>
+                            <br />
+                                <label>
+                                    {error}
+                                </label>
+                        </>
+                    ): null
+                }
+
                 <br />
 
-                <button type={'button'} onClick={(e) => {
+                <button type={'button'} onClick={ async (e) => {
                     e.preventDefault()
+                    if (!event.summary) {
+                        setError("Event title can't be blank")
+                    }
+                    else if (!event.start || !event.end) {
+                        setError("Event should have a start and end time")
+                    }
+                    else if (event.start >= event.end) {
+                        setError("Event end time should be after the event's start time")
+                    }
+                    else {
+                        
+                        setError("");
+                    }
                     console.log(event)
                 }}>Submit</button>
             </form>
