33c9f433448534a871387625f4d70c8e0cb31c26
zAinAli-exploiter <51944161+zAinAli-exploiter@users.noreply.github.com>
2023-11-21 22:25:01 +0530

    design of login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,36 +9,33 @@ export default function Login() {
 
 
     return (
-        <div className={"px-11 py-48 bg-violet-950"}>
-        <div className="flex items-center">
-        <Image src={'/illustration-planning (1).svg'} width={550} height={100} alt={"Sign in icon"} />
+        <section className=" min-h-screen flex items-center justify-center bg-gray-200">
+
+            <div className="flex bg-gray-100 ">
+
+                <div className="px-12 py-4 rounded-2xl items-end justify-center text-center shadow-xl">
+
+                    <p className=" font-serif text-gray-600 py-8">Sign in to calendar</p>
         
-       
-        <div className={"w-96 px- 24 shadow-lm bg-gradient-to-tr from-violet-700 to-indigo-900 rounded-md"}>
-        
-        <h1 className="px-24 py-20 text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-white to to-zinc-400">SIGN IN</h1>
-        <p className="px-36"><Image src={'/user-profile (1).svg'} width={100} height={10} alt={"Sign in icon"} /></p>
-       
-        <div className={"flex justify-items-end w-96 px- 24 shadow-lm bg-gradient-to-bl from-violet-700 to-indigo-900"}>
-        
-        <button className=" px-28 py-20" onClick={async (event) => {
-            await supabase.auth.signInWithOAuth({
-                provider: 'google',
-                options: {
-                    redirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`,
-                    scopes: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
-                }
-            })
-        }}>
-            <Image src={'/googleSignIn.svg'} width={181} height={40} alt={"Sign in with google"} />
-        </button>
-        </div>
-        </div>
-        </div>
-        </div>
-        
+                    <p className=" flex justify-center pb-6 "><Image src={'/calendar.svg'} width={80} height={40} alt={"calendar_icon"} /></p>
+
+                    <button className=" py-8" onClick={async (event) => {
+                        await supabase.auth.signInWithOAuth({
+                            provider: 'google',
+                            options: {
+                                redirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`,
+                                scopes: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
+                            }
+                        })
+                    }}>
+                        <Image src={'/googleSignIn.svg'} width={181} height={40} alt={"Sign in with google"} />
+                    </button>
+                </div>
+
+            </div>
+
+
+       </section> 
     )
 }
 
-
-//<div className={"flex justify-center py-80 items-center"}>
