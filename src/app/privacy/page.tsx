108c510f3d18e311ead33a2f378f89b1dc846186
SteakFisher <miniminter2018p@gmail.com>
2023-11-24 01:20:22 +0530

    TOS and priv policy for google

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,13 @@
+export default function Privacy() {
+    return (
+        <>
+            <h1>Privacy Policy</h1>
+            Honestly just writing this for google's sake, but everything I've written is true (obv)
+            <ul>
+                <li>We collect no data, everything we store is the minimum necessary for the functioning of the app</li>
+                <li>All personal and sensitive tokens accessed by us are stored locally on your browser</li>
+                <li>I'll be a good boy :)</li>
+            </ul>
+        </>
+    )
+}
---
+++ src/app/tos/page.tsx
@@ -0,0 +1,13 @@
+export default function TOS() {
+    return (
+        <>
+            <h1>Terms of Service</h1>
+            By using the service you agree to
+            <ul>
+                <li>Report any security flaws discovered</li>
+                <li>Not use it for any sort of malicious purposes</li>
+                <li>Not be stupid</li>
+            </ul>
+        </>
+    )
+}
