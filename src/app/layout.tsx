2ef8a58bc9fbc8f109058803a777ed37b15c3437
SteakFisher <miniminter2018p@gmail.com>
2023-11-24 14:07:15 +0530

    Fixed build fails

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import {Head} from "next/document";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta name="google-site-verification" content="gY8XbUrGrstYF6_GWFfTO2DeA2gTL4SoKnc-njBM3KY" />
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
 import Link from "next/link";
+import {Head} from "next/document";
 
 export default function Home() {
   return (
-    <>
-      <h1>Remindr.</h1>
-      <h2>Simplifying punctuality</h2>
-      <Link href={"/privacy"}>Privacy</Link>
-      <Link href={"/tos"}>TOS</Link>
-    </>
+      <div>
+          <h1>Remindr.</h1>
+          <h2>Simplifying punctuality</h2>
+          <Link href={"/privacy"}>Privacy</Link>
+          <Link href={"/tos"}>TOS</Link>
+      </div>
   )
 }
